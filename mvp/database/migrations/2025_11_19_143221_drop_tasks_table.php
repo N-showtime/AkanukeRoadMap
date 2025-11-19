44aa6745dc4c644b319a941eda7f4c8cd00976e6
Shogo.N <hozukl.1225@gmail.com>
2025-11-19 15:16:09 +0900

    既存のtasksテーブルを破棄

---
+++ mvp/database/migrations/2025_11_19_143221_drop_tasks_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::dropIfExists('tasks'); // tasks テーブルを削除
+    }
+
+    public function down(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+};
